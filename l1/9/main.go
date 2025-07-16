7966935907e7edc7bda07b27dab7b4e6c022112f
Alexei Lapin <lapin.alx@yandex.ru>
2025-07-16 22:18:14 +0300

    add l1/9 task

---
+++ l1/9/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	ints := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go func() {
+		for i := range ch1 {
+			ch2 <- int(math.Pow(float64(i), 2))
+		}
+
+		close(ch2)
+	}()
+
+	for _, v := range ints {
+		ch1 <- v
+	}
+
+	close(ch1)
+
+	for i := range ch2 {
+		fmt.Println("Received", i, "from ch1")
+	}
+}
