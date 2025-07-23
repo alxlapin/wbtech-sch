da8f82f68c2631e909bd969785ef44a445002f74
Alexei Lapin <lapin.alx@yandex.ru>
2025-07-23 23:01:40 +0300

    add l2/08 task

---
+++ l2/08-wbtech-sch-ntp/go.mod
@@ -0,0 +1,10 @@
+module 08-wbtech-sch-ntp
+
+go 1.24.4
+
+require github.com/beevik/ntp v1.4.3
+
+require (
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
---
+++ l2/08-wbtech-sch-ntp/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Println(os.Stderr, err.Error())
+
+		os.Exit(1)
+	}
+
+	fmt.Println(time)
+}
